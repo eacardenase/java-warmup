28b62d19578734325193ed578bc7cef851e87fa9
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-08-21 18:01:32 -0500

    vargar parameters

--- src/Person.java
+++ src/Person.java
@@ -9,9 +9,16 @@ public class Person {
         System.out.println(message);
     }
 
+    public void test(int num1, int num2, String... words) {
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(words);
+    }
+
     public static void main(String[] args) {
         Person person = new Person();
         person.sayHello();
         person.saySomething("I'm happy learning Java!");
+        person.test(0, 1, "two", "three", "four");
     }
 }

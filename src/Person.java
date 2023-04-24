8d99548fa8b059979d49b8d9515fc1e2dcdc5b11
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-24 10:03:10 -0500

    feat: working with methods

--- src/Person.java
+++ src/Person.java
@@ -3,9 +3,7 @@ import business.Company;
 import java.time.LocalDate;
 
 public class Person {
-    private String firstName;
-    private String lastName;
-    private LocalDate dob;
+    private String middleName = "Alexander";
 
     public void sayHello() {
         System.out.println("Hello");
@@ -13,5 +11,26 @@ public class Person {
 
     public void saySomething(String message) {
         System.out.println(message);
+    }
+
+    public char getMiddleInitial() {
+        return middleName.charAt(0);
+    }
+
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.sayHello();
+        person.saySomething("You are fully capable of deciding your own destiny.");
+        person.saySomething("The question you face is:");
+        person.saySomething("Which path will you choose?");
+
+        System.out.println(person.getMiddleInitial());
+
+        System.out.println(person.add(2, 5));
     }
 }

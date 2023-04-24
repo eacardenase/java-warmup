305a13ddd2795dd1b401ed8af9a662b0a7e436d2
Edwin Cardenas <eacardenase@unal.edu.co>
2023-04-23 21:12:18 -0500

    feat: variables and data types

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+import business.Company;
+
+public class Person {
+    private final double PI = 3.14;
+    private String firstName;
+    private int age;
+    private long id;
+    private char middleInitial;
+    private byte myByte;
+    private Company company;
+    private NewsAgency agency;
+}
